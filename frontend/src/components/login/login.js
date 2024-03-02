35f9a6595de9792c87f58feeb8a7dc694412fb05
Mike Lin <mastermaiku3@gmail.com>
2024-03-02 14:11:41 -0500

    Add Auth0 authentication to login component and configure Auth0Provider in index.js

--- frontend/src/components/login/login.js
+++ frontend/src/components/login/login.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./login.css"
+import {useAuth0} from "@auth0/auth0-react";
 
-function Login({handleLogin}) {
+const Login = () => {
+    const {logInWithRedirect, isAuthenticated} = useAuth0();
     // Component body
     return (
-        <div className="login-container">
-            <button className="login-button" onClick={handleLogin}>Login</button>
-        </div>
+        !isAuthenticated && (
+            <div className="login-container">
+            <button className="login-button" onClick={() => logInWithRedirect()}>
+            Login
+            </button>
+            </div>
+        )
     );
 }
 

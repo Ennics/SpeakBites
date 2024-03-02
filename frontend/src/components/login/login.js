9ee0d657132d5749e35a8de01b02e553af6b91fc
Nicholas Nicolaev <84345327+Ennics@users.noreply.github.com>
2024-03-02 14:35:58 -0500

    Added basic login.

--- frontend/src/components/login/login.js
+++ frontend/src/components/login/login.js
@@ -1,18 +1,12 @@
 import React from "react";
 import "./login.css"
-import {useAuth0} from "@auth0/auth0-react";
 
-const Login = () => {
-    const {logInWithRedirect, isAuthenticated} = useAuth0();
+function Login({handleLogin}) {
     // Component body
     return (
-        !isAuthenticated && (
-            <div className="login-container">
-            <button className="login-button" onClick={() => logInWithRedirect()}>
-            Login
-            </button>
-            </div>
-        )
+        <div className="login-container">
+            <button className="login-button" onClick={handleLogin}>Login</button>
+        </div>
     );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {Autho0Provider} from '@auth0/auth0-react';
+// import {Autho0Provider} from '@auth0/auth0-react';
 
 
-const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+// const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+// const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Autho0Provider domain={domain} clientId={clientId} redirectUri={window.location.origin}>
+    {/* <Autho0Provider domain={domain} clientId={clientId} redirectUri={window.location.origin}> */}
       <App />
-    </Autho0Provider>
+    {/* </Autho0Provider> */}
   </React.StrictMode>
 );
 

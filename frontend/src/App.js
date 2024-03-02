8a9ee170bf1213cca11eedd8c771cce5bd8bd6f5
Nicholas Nicolaev <84345327+Ennics@users.noreply.github.com>
2024-03-02 13:54:23 -0500

    Reconfigured component structure.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,29 @@
 import React from "react";
+import { useState } from "react";
+
+import ControlPanel from "./components/controlPanel/controlPanel";
 import Login from "./components/login/login";
 
 function App() {
+  // State variables
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  // Handler functions
+  const handleLogin = () => {
+    setLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setLoggedIn(false);
+  };
+
   return (
     <div>
-      <Login/>
+      {!loggedIn ? (
+          <Login handleLogin={handleLogin}/>
+      ) : (
+          <ControlPanel handleLogout={handleLogout}/>
+      )}
     </div>
   );
 }
--- frontend/src/components/login/login.js
+++ frontend/src/components/login/login.js
@@ -1,30 +1,11 @@
 import React from "react";
-import { useState } from "react";
 import "./login.css"
 
-import ControlPanel from "../controlPanel/controlPanel";
-
-function Login() {
-    // State variables
-    const [loggedIn, setLoggedIn] = useState(false);
-
-    // Handler functions
-    const handleLogin = () => {
-        setLoggedIn(true);
-    };
-
-    const handleLogout = () => {
-        setLoggedIn(false);
-    };
-
+function Login({handleLogin}) {
     // Component body
     return (
         <div className="login-container">
-            {!loggedIn ? (
-                <button className="login-button" onClick={handleLogin}>Login</button>
-            ) : (
-                <ControlPanel handleLogout={handleLogout}/>
-            )}
+            <button className="login-button" onClick={handleLogin}>Login</button>
         </div>
     );
 }
